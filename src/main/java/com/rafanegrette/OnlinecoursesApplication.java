adacd6b4f174dfe7c0233d0b59015ef5bcc1e99c
Rafa Negrette <rafael.negrette@stackpointer.co>
2019-02-28 18:37:22 -0500

    Initial commit

---
+++ src/main/java/com/rafanegrette/OnlinecoursesApplication.java
@@ -0,0 +1,13 @@
+package com.rafanegrette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecoursesApplication.class, args);
+	}
+
+}
